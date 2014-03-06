8b817acd349ba761b19bb8e2fa7353a9f037e934
Craig Anderson <craig@coot.net>
2014-03-06 15:13:11 -0700

    push and then test bundle install on raspberry pi

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "http://rubygems.org"
+gem "reel"
+gem "log4r"
+gem 'yard'
+gem 'jruby-openssl'
+gem "htmlentities"
+gem "json"
+gem "haml"
+
---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'log4r'
+require 'log4r/yamlconfigurator'
+require 'celluloid/autostart'
+require 'htmlentities'
+
+require_relative './version'
+
+module ReelHttpsAuthWebsock
+end
+
